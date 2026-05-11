6a45baac08065f3031bdadb94843e1ed8d754ca5
Cesar <caugustorivero@unicesar.edu.co>
2026-05-10 19:12:52 -0500

    feat(dao): add IUsuarioDAO interface

---
+++ MediConsult-Beta/src/com/mediconsult/dao/interfaces/IUsuarioDAO.java
@@ -0,0 +1,12 @@
+package com.mediconsult.dao.interfaces;
+
+import com.mediconsult.model.Usuario;
+import java.util.Optional;
+
+/**
+ * Contrato para autenticacion de usuarios.
+ * Interfaz minima: solo expone lo necesario para el login.
+ */
+public interface IUsuarioDAO {
+    Optional<Usuario> buscarPorUsername(String username);
+}
